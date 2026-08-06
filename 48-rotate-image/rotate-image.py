c7d2d6b485fdb11e8ee5e61dd00c7f5f38fb179e
Abhijit <official.abhijit53@gmail.com>
2026-08-06 10:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.04%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        # Transpose the matrix
+        for row in range(n):
+            for col in range(row + 1, n):
+                matrix[row][col], matrix[col][row] = (
+                    matrix[col][row],
+                    matrix[row][col]
+                )
+
+        # Reverse every row
+        for row in matrix:
+            row.reverse()
