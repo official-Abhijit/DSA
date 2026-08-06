74092f44a724929f646b8c704e2d92db2c858e19
Abhijit <official.abhijit53@gmail.com>
2026-08-06 10:13:39 +0530

    Time: 2 ms (78.95%) | Memory: 19.3 MB (73.50%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for row in range(9):
+            for col in range(9):
+                value = board[row][col]
+
+                if value == ".":
+                    continue
+
+                box_index = (row // 3) * 3 + (col // 3)
+
+                if (
+                    value in rows[row]
+                    or value in cols[col]
+                    or value in boxes[box_index]
+                ):
+                    return False
+
+                rows[row].add(value)
+                cols[col].add(value)
+                boxes[box_index].add(value)
+
+        return True
