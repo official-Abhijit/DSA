89f13b07b3c4009934692a96b0a1e9572d850e17
Abhijit <official.abhijit53@gmail.com>
2026-08-07 19:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (5.10%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        current = []
+
+        def backtrack(start):
+            result.append(current.copy())
+
+            for i in range(start, len(nums)):
+                current.append(nums[i])
+                backtrack(i + 1)
+                current.pop()
+
+        backtrack(0)
+        return result
