cb3cc53e2d55f99669eaf0c5c969cd556e480ce9
Abhijit <official.abhijit53@gmail.com>
2026-08-07 19:17:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (60.31%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        mask = 1
+
+        while mask <= num:
+            mask <<= 1
+
+        mask -= 1
+
+        return num ^ mask
