593f2b4acfa34920f63cfc3cd1340618dfd554d5
Abhijit <official.abhijit53@gmail.com>
2026-08-13 13:21:59 +0530

    Time: 22 ms (74.44%) | Memory: 20.8 MB (64.18%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.py
@@ -0,0 +1,13 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
+
+        max_freq = max(freq)
+        max_count = freq.count(max_freq)
+
+        slots = (max_freq - 1) * (n + 1) + max_count
+
+        return max(len(tasks), slots)
